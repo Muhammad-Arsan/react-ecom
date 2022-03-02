c428ad5759091c439933a964d20d19ffee595b6b
Muhammad-Arsan <arsan924@gmail.com>
2022-03-02 08:56:48 +0500

    home screen

--- frontend/src/components/screens/HomeScreen/HomeScreen.js
+++ frontend/src/components/screens/HomeScreen/HomeScreen.js
@@ -2,19 +2,26 @@ import React from "react";
 import Product from "../../Product/Product";
 import "./HomeScreen.css";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getProducts } from "../../../redux/actions/productActions";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts as listProducts } from "../../../redux/actions/productActions";
 const HomeScreen = () => {
   const dispatch = useDispatch();
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
   useEffect(() => {
-    dispatch(getProducts());
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
   return (
     <div className="homescreen">
       <h2 className="homescreen__title">Latest Pictures</h2>
       <div className="homescreen__products">
-        <Product />
-        <Product />
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : error ? (
+          <h2>{error}</h2>
+        ) : (
+          products.map((product) => <Product />)
+        )}
       </div>
     </div>
   );
